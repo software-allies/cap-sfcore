d92d91a4540a4823cdad2f5ddccfebe49a059f31
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-01-31 09:54:48 -0600

    CAP-310 src/lib/services/ create the service that provides the components to communicate with the API and import all the components in the main module.

---
+++ src/lib/sales-force.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { LoopbackRoutingModule } from './loopback-routing.module';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { LoopbackService } from './services/loopback.service';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { IndexComponent } from './components/index/index.component';
+import { AccountSFComponent } from './components/account-sf/account-sf.component';
+import { ContactSFComponent } from './components/contact-sf/contact-sf.component';
+import { LeadSFComponent } from './components/lead-sf/lead-sf.component';
+import { OpportunitySFComponent } from './components/opportunity-sf/opportunity-sf.component';
+
+@NgModule({
+  declarations: [
+    PaginationComponent,
+    IndexComponent,
+    AccountSFComponent,
+    ContactSFComponent,
+    LeadSFComponent,
+    OpportunitySFComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    BrowserAnimationsModule,
+    LoopbackRoutingModule
+  ],
+  exports: [
+    PaginationComponent,
+    IndexComponent,
+    AccountSFComponent,
+    ContactSFComponent,
+    LeadSFComponent,
+    OpportunitySFComponent
+  ],
+  providers: [LoopbackService]
+})
+export class LoopbackModule {}
---
+++ src/lib/services/loopback.service.ts
@@ -0,0 +1,119 @@
+import { Injectable , Inject, PLATFORM_ID } from "@angular/core";
+import { isPlatformBrowser } from '@angular/common';
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { map } from "rxjs/operators";
+import { JwtHelperService } from "@auth0/angular-jwt";
+
+@Injectable()
+export class LoopbackService {
+  url: string;
+  limit: number;
+  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId) {
+    this.url = 'https://heroku-app-api.herokuapp.com/api';
+    this.limit = 20;
+  }
+
+  getToken(): string {
+    if (isPlatformBrowser(this.platformId) && localStorage.getItem('User')) {
+      return JSON.parse(localStorage.getItem('User')).token;
+    }
+  }
+
+  isUserLoggedIn(): boolean {
+    if (isPlatformBrowser(this.platformId) && localStorage.getItem('User')) {
+      const userStorage = JSON.parse(localStorage.getItem('User'));
+      const helper = new JwtHelperService();
+      if (!helper.isTokenExpired(userStorage.token)) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  getAllRequest(tableName: string, offset: number = 0) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${this.getToken()}`
+      })
+    };
+    return this.http.get(`${this.url}/${tableName}?filter={"where":{"UUID__c": {"nlike": "null" }},"order":"Name","limit":${this.limit},"offset":${offset}}`, httpOptions);
+  }
+
+  getWithFilter(query: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${this.getToken()}`
+      })
+    };
+    return this.http.get(`${this.url}/${query}`, httpOptions);
+  }
+
+  getTotalItems(tableName: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${this.getToken()}`
+      })
+    };
+    return this.http.get(`${this.url}/${tableName}/count?where={"UUID__c": {"nlike": "null" }}`, httpOptions)
+      .pipe(map( (data: any) => {
+          return data.count;
+        })
+      );
+  }
+
+  getRecordWithFindOne(tableName: string, sfid: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${this.getToken()}`
+      })
+    };
+    return this.http.get(`${this.url}/${tableName}/findOne?filter={"where":{"SfId":"${sfid}"}}`, httpOptions);
+  }
+
+  getRecordRequest(tableName: string, id: number) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${this.getToken()}`
+      })
+    };
+    return this.http.get(`${this.url}/${tableName}/${id}`, httpOptions);
+  }
+
+  postRequest(tableName: string, body: object) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${this.getToken()}`
+      })
+    };
+    return this.http.post(`${this.url}/${tableName}`, body, httpOptions);
+  }
+
+  patchRequest(tableName: string, id: number, body: object) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${this.getToken()}`
+      })
+    };
+    return this.http.patch(`${this.url}/${tableName}/${id}`, body, httpOptions);
+  }
+
+  deleteItem(tableName: string, id: number) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${this.getToken()}`
+      })
+    };
+    return this.http.delete(`${this.url}/${tableName}/${id}`, httpOptions);
+  }
+}
