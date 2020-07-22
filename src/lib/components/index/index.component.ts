835886f5e51c0da60bac7132d64eb0fc1e995af4
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-07-22 13:53:15 -0500

    CAP-608 CAP-609 CAP-607 src/lib/components/index/index.component.ts Remove pickList to serch records and fix the navigations between SalesForce objects if before serarched a record.

--- src/lib/components/index/index.component.ts
+++ src/lib/components/index/index.component.ts
@@ -15,30 +15,35 @@ import Swal from 'sweetalert2';
         <div class="row">
 
           <div class="col-md-3 mb-2">
-            <a routerLink="/{{objectComponent}}/create" class="btn btn-success pull-right">
+            <a routerLink="/{{objectComponent}}/create" class="btn btn-outline-dark pull-right">
               Add New {{objectComponent | titlecase}}
             </a>
           </div>
 
           <div class="col-md-9">
             <div class="form-row">
+
+            <!--
               <div class="col-md-3 mb-2">
                 <select class="custom-select" [(ngModel)]="selectAttribute">
                   <option *ngFor="let search of object.search; let i = index" value="{{search}}">{{search}}</option>
                 </select>
               </div>
-              <div class="input-group col-md-9 mb-2">
-                <input type="text" [(ngModel)]="searchAttribute" class="form-control">
+            -->
+
+              <div class="input-group col-md-12 mb-2">
+                <input type="text" [(ngModel)]="searchAttribute" class="form-control" (keyup.enter)="searchBy()">
                 <div class="input-group-append">
-                  <button (click)="searchBy()" class="btn btn-outline-secondary" type="button"> Search </button>
+                  <button (click)="searchBy()" class="btn btn-outline-dark" type="button"> Search </button>
                 </div>
               </div>
+
             </div>
           </div>
         </div>
       </div>
-        <table *ngIf="listings.length && !listings404" class=" tbl-header table table-striped table-condensed mt-3 rwd_auto">
-          <thead>
+        <table *ngIf="listings.length && !listings404" class="table table-borderless table-hover rwd_auto mt-3">
+          <thead class="thead-dark">
             <tr *ngIf="objectComponent === 'account'">
               <th scope="col">Account Name</th>
               <th scope="col" class="discard">Billing City</th>
@@ -67,14 +72,14 @@ import Swal from 'sweetalert2';
           <tbody>
             <tr *ngFor="let object of listings | paginate: { id: 'pagination', itemsPerPage: totalListings, currentPage: currentPage, totalItems: totalItems}">
 
-              <td *ngIf="objectComponent === 'account'">
+              <td *ngIf="objectComponent === 'account'" scope="row">
                 <a routerLink="/{{objectComponent}}/{{object.SfId}}">{{ object.Name }}</a>
               </td>
               <td *ngIf="objectComponent === 'account'" class="discard">{{ object.BillingCity }}</td>
               <td *ngIf="objectComponent === 'account'" class="numeric discard">{{ object.Phone }}</td>
 
 
-              <td *ngIf="objectComponent === 'contact'">
+              <td *ngIf="objectComponent === 'contact'" scope="row">
                 <a routerLink="/{{objectComponent}}/{{object.SfId}}">{{ object.FirstName }} {{object.LastName}}</a>
               </td>
               <td *ngIf="objectComponent === 'contact'" class="discard">
@@ -83,7 +88,7 @@ import Swal from 'sweetalert2';
               <td *ngIf="objectComponent === 'contact'" class="numeric discard">{{ object.MobilePhone }}</td>
 
 
-              <td *ngIf="objectComponent === 'opportunity'">
+              <td *ngIf="objectComponent === 'opportunity'" scope="row">
                 <a routerLink="/{{objectComponent}}/{{object.SfId}}">{{ object.Name }}</a>
               </td>
               <td *ngIf="objectComponent === 'opportunity'" class="discard">
@@ -92,7 +97,7 @@ import Swal from 'sweetalert2';
               <td *ngIf="objectComponent === 'opportunity'" class="date discard">{{ object.CloseDate | date: 'MM/dd/yyyy' : 'UTC/GMT'}}</td>
 
 
-              <td *ngIf="objectComponent === 'lead'">
+              <td *ngIf="objectComponent === 'lead'"  scope="row">
                 <a routerLink="/{{objectComponent}}/{{object.SfId}}">{{ object.FirstName }} {{object.LastName}}</a>
               </td>
               <td *ngIf="objectComponent === 'lead'" class="discard">{{ object.Company }}</td>
@@ -100,10 +105,11 @@ import Swal from 'sweetalert2';
 
               <td class="text-center">
                 <div class="dropdown">
-                  <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="actions" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <button class="btn btn-dark btn-sm dropdown-toggle" type="button" id="actions" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fa fa-ellipsis-h"></i>
                   </button>
                   <div class="dropdown-menu" aria-labelledby="actions">
+                    <button class="dropdown-item" type="button" routerLink="/{{objectComponent}}/{{ object.SfId }}">View</button>
                     <button class="dropdown-item" type="button" routerLink="/{{objectComponent}}/{{ object.SfId }}/update">Update</button>
                     <button class="dropdown-item" type="button" (click)="deleteItem(object.id)">Delete</button>
                   </div>
@@ -121,9 +127,9 @@ import Swal from 'sweetalert2';
 
     <div  *ngIf="!listings.length && listings404" class="card text-center mt-3">
       <div class="card-body">
-        <h5 class="card-title">No records found.</h5>
+        <h5 class="card-title">No Results found!</h5>
         <p class="card-text">
-          No records found found in {{objectComponent | titlecase}} object, try again later or check your organization info in SalesForce.
+          Sorry, but nothing matched your search terms. Please try again with some different keywords.
         </p>
       </div>
     </div>
@@ -132,12 +138,15 @@ import Swal from 'sweetalert2';
 
   `,
   styles: [`
+  table {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  }
 
-  table.rwd_auto { border: 1px solid #ccc; width: 100%; margin: 0 0 50px 0; }
-  .rwd_auto th { background:#ccc; padding:5px; text-align: left; }
+  table.rwd_auto {width: 100%; margin: 0 0 50px 0; }
+  .rwd_auto th {padding:5px; text-align: left; }
   .rwd_auto th:last-child {text-align: center; }
 
-  .rwd_auto td { border-bottom: 1px solid #ccc; padding: 5px; text-align: left; }
+  .rwd_auto td { border-bottom: .5px solid #ccc; padding: 5px; text-align: left; }
   .rwd_auto td.numeric { text-align: left; }
   .rwd_auto td.date { text-align: left; }
   .rwd_auto tr:last-child td { border:0; }
@@ -183,7 +192,6 @@ export class IndexComponent implements OnInit {
       api: 'Accounts',
       search: [
         'Name',
-        'SfId'
       ]
     },
     {
@@ -191,7 +199,6 @@ export class IndexComponent implements OnInit {
       api: 'Contacts',
       search: [
         'FirstName',
-        'SfId'
       ]
     },
     {
@@ -199,7 +206,6 @@ export class IndexComponent implements OnInit {
       api: 'Leads',
       search: [
         'FirstName',
-        'SfId'
       ]
     },
     {
@@ -207,7 +213,6 @@ export class IndexComponent implements OnInit {
       api: 'Opportunitys',
       search: [
         'Name',
-        'SfId'
       ]
     }
   ];
@@ -246,6 +251,7 @@ export class IndexComponent implements OnInit {
     });
 
     this.activateRoute.params.subscribe(params => {
+      this.resetSearch();
       this.object = this.objects.find(x => x.object === params.object);
       this.objectAPI = this.object.api;
       this.objectComponent = this.object.object;
@@ -290,8 +296,9 @@ export class IndexComponent implements OnInit {
   searchBy() {
     if (this.searchAttribute) {
       this.resetFiltersByFindOne();
-      this.loopBackService.getByFindOneSearch(this.objectAPI, this.selectAttribute, this.searchAttribute).subscribe((listing: any) => {
-        this.listings = [listing];
+      this.loopBackService.getByFindOneSearch(this.objectAPI, this.selectAttribute, this.searchAttribute).subscribe((listings: any) => {
+        this.listings = listings;
+        this.listings404 = listings.length < 1 ? true : false;
       }, (error: any) => {
         this.listings404 = true;
         console.log(error.status + ' - ' + error.statusText);
@@ -300,7 +307,6 @@ export class IndexComponent implements OnInit {
       this.resetFilters();
       this.search();
     }
-
   }
 
   resetFilters() {
@@ -308,6 +314,8 @@ export class IndexComponent implements OnInit {
     this.skipFilter = 0;
     this.currentPage = 1;
     this.listings404 = false;
+    // this.searchAttribute = '';
+    // this.selectAttribute = '';
     // this.totalItems = null;
   }
 
@@ -317,6 +325,12 @@ export class IndexComponent implements OnInit {
     this.currentPage = 1;
     this.totalItems = null;
     this.listings404 = false;
+  }
+
+  resetSearch() {
+    this.listings404 = false;
+    this.searchAttribute = '';
+    this.selectAttribute = '';
   }
 
   deleteItem(id: number) {
