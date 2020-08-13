363f8dd3962a9e8eb0a531a39df277bc60ece939
Diego SanMo <diego14290764@itcg.edu.mx>
2020-08-13 15:11:57 -0500

    The redirect when the opportunity is updated has been changed.

--- src/lib/components/opportunity-sf/opportunity-sf.component.ts
+++ src/lib/components/opportunity-sf/opportunity-sf.component.ts
@@ -107,20 +107,8 @@ export class OpportunitySFComponent implements OnInit {
 
   accounts: any[] = [];
   contacts: any[] = [];
-  limit: number = 10;
-  config = {
-    displayKey: "description", //if objects array passed which key to be displayed defaults to description
-    search: true, //true/false for the search functionlity defaults to false,
-    height: 'auto', //height of the list so that if there are more no of items it can show a scroll defaults to auto. With auto height scroll will never appear
-    placeholder: 'Select', // text to be displayed when no item is selected defaults to Select,
-    limitTo: this.limit, // a number thats limits the no of options displayed in the UI similar to angular's limitTo pipe
-    moreText: 'more', // text to be displayed whenmore than one items are selected like Option 1 + 5 more
-    noResultsFound: 'No results found!', // text to be displayed when no items are found while searching
-    searchPlaceholder: 'Search', // label thats displayed in search input,
-    searchOnKey: 'name', // key on which search should be performed this will be selective search. if undefined this will be extensive search on all keys
-    clearOnSelection: false, // clears search criteria when an option is selected if set to true, default is false
-    inputDirection: 'ltr' // the direction of the search input can be rtl or ltr(default)
-  };
+  
+  paramID: string = '';
 
   constructor(
     private activateRoute: ActivatedRoute,
@@ -147,6 +135,7 @@ export class OpportunitySFComponent implements OnInit {
       this.createForm();
     } else {
       this.activateRoute.params.subscribe((params: { id: string, status?: string }) => {
+        this.paramID = params.id;
         this.getObject(params.id);
         this.status = params.status === 'update' ? true : false;
       });
@@ -315,7 +304,7 @@ export class OpportunitySFComponent implements OnInit {
                 title: 'Your opportunity has been saved',
                 showConfirmButton: false,
                 timer: 1500
-              }).then(result => window.location.assign(`${window.location.origin}/opportunity`));
+              }).then(result => window.location.assign(`${window.location.origin}/opportunity/${this.paramID}`));
             }
           });
 
